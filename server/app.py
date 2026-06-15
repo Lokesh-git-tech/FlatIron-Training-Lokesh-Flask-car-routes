4fb7a7f1cf82d8c41e58f1d474fd3d22b52d6cfd
Lokesh <lokeshkumar.patel@genpact.com>
2026-06-15 14:06:19 +0000

    Push changes made to app.py

--- server/app.py
+++ server/app.py
@@ -1,1 +1,19 @@
 existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
+
+from flask import Flask
+
+# 1. Initialize the Flask application
+app = Flask(__name__)
+
+# 2. Define the home page route
+@app.route("/")
+def home():
+    return "Welcome to Flatiron Cars"
+
+@app.route("<model>")
+def show_model(model):
+    if model in existing_models:
+        print (f"Flatiron {model} is in our fleet!")
+    else:
+        print (f"No models called {model} exists in our catalog.")
+
